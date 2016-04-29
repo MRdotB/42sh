2c582bbff4476fa7104c7c50683abfe215e2a8cd
Baptiste CHALEIL <bchaleil@e3r3p2.42.fr>
2016-04-29 13:13:49 +0200

    Update router

--- srcs/router.c
+++ srcs/router.c
@@ -6,7 +6,7 @@
 /*   By: bchaleil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/28 16:39:58 by bchaleil          #+#    #+#             */
-/*   Updated: 2016/04/28 19:42:48 by bchaleil         ###   ########.fr       */
+/*   Updated: 2016/04/29 13:01:19 by bchaleil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,15 @@ void	router(char *line)
 {
 	int			i;
 	char		**entry;
-	static void	(*f[6])(char **entry) =
-	{bc_exit };
+	static void	(*f[7])(char **entry) =
+	{ bc_cd, bc_exit, bc_clear };
 	static char	*bultins[] =
-	{"cd", "env", "setenv", "unsetenv", "exit", "pwd" };
+	{"cd", "exit", "clear", "env", "setenv", "unsetenv", "exit", "pwd" };
 
 	printf("DEBUG line: %s\n", line);
 	entry = ft_strsplit(line, ' ');
 	i = 0;
-	while (i < 6)
+	while (i < 7)
 	{
 		if (ft_strcmp(bultins[i], entry[0]) == 0)
 			f[i](entry);
