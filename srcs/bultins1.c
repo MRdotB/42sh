6fc225f3162976e1671564f28d490fd04abc653d
Baptiste CHALEIL <bchaleil@e3r5p5.42.fr>
2016-05-02 14:19:22 +0200

    bultins done

--- srcs/bultins1.c
+++ srcs/bultins1.c
@@ -6,43 +6,43 @@
 /*   By: bchaleil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/29 13:10:46 by bchaleil          #+#    #+#             */
-/*   Updated: 2016/04/29 15:43:32 by bchaleil         ###   ########.fr       */
+/*   Updated: 2016/05/02 14:05:11 by bchaleil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "42sh.h"
 
-char	**env_cpy;
+t_env	*g_head;
 
 void	bc_cd(char **path)
 {
 	chdir(path[1]);
 }
+
 void	bc_exit(char **path)
 {
-	int	i;
-
-	i = 0;
-	while (env_cpy[i])
-		free(env_cpy[i++]);
-	free(env_cpy);
 	if (*path)
+	{
+		clean_env();
 		exit(0);
+	}
 }
 
 void	bc_clear(char **path)
 {
-	const char*	clear_screen_ansi;
+	const char	*clear_screen_ansi;
 
 	clear_screen_ansi = "\e[1;1H\e[2J";
-	if (*path)
-		ft_putstr_fd(clear_screen_ansi, STDOUT_FILENO);
+	if (path[1])
+		return (bc_error("clear: too much args."));
+	ft_putstr_fd(clear_screen_ansi, STDOUT_FILENO);
 }
 
 void	bc_pwd(char **path)
 {
 	char current[PATH_MAX];
 
-	if (*path)
-		ft_putendl(getcwd(current, PATH_MAX));
+	if (path[1])
+		return (bc_error("pwd: too much args."));
+	ft_putendl(getcwd(current, PATH_MAX));
 }
--- srcs/bultins2.c
+++ srcs/bultins2.c
@@ -6,117 +6,44 @@
 /*   By: bchaleil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/29 13:12:56 by bchaleil          #+#    #+#             */
-/*   Updated: 2016/04/29 18:30:32 by bchaleil         ###   ########.fr       */
+/*   Updated: 2016/05/02 13:57:05 by bchaleil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "42sh.h"
 
-char	**env_cpy;
-
-char	**cpy_env(char **environ, int add, int del)
-{
-	char	**env_cpy;
-	int		i;
-
-	i = 0;
-	while(environ[i])
-		i++;
-	if (!(env_cpy = (char**)ft_memalloc(sizeof(char*) * i + add + 1)))
-		return (NULL);
-	i = 0;
-	while(environ[i])
-	{
-		env_cpy[i + add] = ft_strdup(environ[i]);
-		if (del)
-			free(environ[i]);
-		i++;
-	}
-	env_cpy[i] = NULL;
-	if (del)
-		free(environ);
-	return (env_cpy);
-}
-
 void	bc_env(char **path)
 {
-	int	i;
-
-	i = 0;
-	if (*path)
-		while (env_cpy[i])
-		{
-			if (ft_strcmp(env_cpy[i], "_skip_") != 0)
-				ft_putendl(env_cpy[i]);
-			i++;
-		}
+	if (path[1])
+		return (bc_error("env: too much args."));
+	display_env();
 }
 
 void	bc_getenv(char **path)
 {
-	int i;
-	int equal_pos;
-
-	i = 0;
 	if (path[1] == NULL)
 		return (bc_error("getenv: missing arg."));
 	else if (path[2])
 		return (bc_error("getenv: too much args."));
-	while (env_cpy[i])
-	{
-		equal_pos = ft_strindexof(env_cpy[i], '=');
-		if (ft_strncmp(path[1], env_cpy[i], equal_pos - 1) == 0)
-			return (ft_putendl(env_cpy[i] + equal_pos));
-		i++;
-	}
-	return (bc_error("getenv: variable not found in env."));
+	if (!(get_env(path[1])))
+		bc_error("getenv: variable not found in env.");
 }
 
 void	bc_unsetenv(char **path)
 {
-	int	i;
-	int	equal_pos;
-
-	i = 0;
-	while (env_cpy[i])
-	{
-		equal_pos = ft_strindexof(env_cpy[i], '=');
-		if (ft_strncmp(path[1], env_cpy[i], equal_pos - 1) == 0)
-		{
-			free(env_cpy[i]);
-			env_cpy[i] = ft_strdup("_skip_");
-			return ;
-		}
-		i++;
-	}
-	bc_error("unsetenv: variable not found in env.");
+	if (path[1] == NULL)
+		return (bc_error("unsetenv: missing arg."));
+	else if (path[2])
+		return (bc_error("unsetenv: too much args."));
+	if (!(unset_env(path[1])))
+		bc_error("unsetenv: variable not found in env.");
 }
 
 void	bc_setenv(char **path)
 {
-	char	*var;
-	int		i;
-	int		equal_pos;
-
-	i = 0;
-	var = ft_concat(path[1], "=", 0);
-	var = ft_concat(var, path[2], 1);
-	while (env_cpy[i])
-	{
-		equal_pos = ft_strindexof(env_cpy[i], '=');
-		if (ft_strncmp(path[1], env_cpy[i], equal_pos - 1) == 0)
-		{
-			if (path[3][0] == '1')
-			{
-				free(env_cpy[i]);
-				env_cpy[i] = var;
-				return ;
-			}
-			else if (path[3][0] == '0')
-				return ;
-		}
-		i++;
-	}
-	env_cpy = cpy_env(env_cpy, 1, 1);
-	env_cpy[0] = var;
+	if (path[1] == NULL || path[2] == NULL || path[3] == NULL)
+		return (bc_error("setenv: missing arg."));
+	else if (path[4])
+		return (bc_error("unsetenv: too much args."));
+	set_env(path[1], path[2], ft_atoi(path[3]));
 }
