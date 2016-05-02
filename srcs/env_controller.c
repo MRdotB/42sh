3a38356f90b58a653e038fe6bae2a98c65f4438a
Baptiste CHALEIL <bchaleil@e3r5p5.42.fr>
2016-05-02 14:20:13 +0200

    env controller done

--- srcs/env_controller.c
+++ srcs/env_controller.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env_controller.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bchaleil <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/05/02 11:09:49 by bchaleil          #+#    #+#             */
+/*   Updated: 2016/05/02 14:04:32 by bchaleil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "42sh.h"
 
-t_env	*head;
+t_env		*g_head;
 extern char	**environ;
 
-t_env	*new_elem(char *env)
+static t_env	*new_elem(char *env)
 {
 	t_env	*r;
 	char	*key;
@@ -22,7 +34,7 @@ t_env	*new_elem(char *env)
 	return (r);
 }
 
-t_env	*push_front(t_env *lst, t_env *el)
+static t_env	*push_front(t_env *lst, t_env *el)
 {
 	if (lst == NULL)
 		return (el);
@@ -31,112 +43,21 @@ t_env	*push_front(t_env *lst, t_env *el)
 	return (el);
 }
 
-void	clean_env(void)
-{
-	t_env	*tmp;
-	while (head)
-	{
-		free(head->key);
-		free(head->value);
-		tmp = head->next;
-		free(head);
-		head = tmp;
-	}
-}
-
-void	display_env(void)
-{
-	t_env	*cpy;
-
-	cpy = head;
-	while (cpy)
-	{
-		ft_putstr(cpy->key);
-		ft_putchar('=');
-		ft_putendl(cpy->value);
-		cpy = cpy->next;
-	}
-}
-
-void	set_env(char *key, char *value, int overwrite)
-{
-	t_env	*cpy;
-
-	cpy = head;
-	while (cpy)
-	{
-		if (overwrite && ft_strcmp(cpy->key, key) == 0)
-		{
-			free(cpy->value);
-			cpy->value = ft_strdup(value);
-			return ;
-		}
-		cpy = cpy->next;
-	}
-	if (!(cpy = ft_memalloc(sizeof(*cpy))))
-		return ;
-	cpy->key = ft_strdup(key);
-	cpy->value = ft_strdup(value);
-	cpy->next = head;
-	head = cpy;
-}
-
-void	get_env(char *key)
-{
-	t_env	*cpy;
-
-	cpy = head;
-	while (cpy)
-	{
-		if (ft_strcmp(cpy->key, key) == 0)
-		{
-			ft_putendl(cpy->value);
-			return ;
-		}
-		cpy = cpy->next;
-	}
-}
-
-void	unset_env(char *key)
-{
-	t_env	*cpy;
-	t_env	*prev;
-
-	cpy = head;
-	prev = NULL;
-	while (cpy)
-	{
-		if (ft_strcmp(cpy->key, key) == 0)
-		{
-			if (prev == NULL)
-				head = head->next;
-			else
-				prev->next = cpy->next;
-			free(cpy->key);
-			free(cpy->value);
-			free(cpy);
-			return ;
-		}
-		prev = cpy;
-		cpy = cpy->next;
-	}
-}
-
-char	**env_to_tab(void)
+char			**env_to_tab(void)
 {
 	t_env	*cpy;
 	int		i;
 	char	**r;
 	char	*el;
 
-	cpy = head;
+	cpy = g_head;
 	i = 0;
 	while (cpy)
 	{
 		i++;
 		cpy = cpy->next;
 	}
-	cpy = head;
+	cpy = g_head;
 	if (!(r = (char**)ft_memalloc(sizeof(char*) * (i + 1))))
 		return (NULL);
 	i = 0;
@@ -150,22 +71,24 @@ char	**env_to_tab(void)
 	return (r);
 }
 
-void	env_init(void)
+void			free_double_tab(char **tab)
+{
+	int	i;
+
+	i = 0;
+	while (tab[i])
+		free(tab[i++]);
+	free(tab);
+}
+
+void			env_init(void)
 {
 	int		i;
 
-	head = NULL;
+	g_head = NULL;
 	i = 0;
 	while (environ[i])
 		i++;
 	while (i > 0)
-		head = push_front(head, new_elem(environ[--i]));
-	set_env("testttt", "value", 1);
-	set_env("carlos", "lol", 1);
-	get_env("carlos");
-	unset_env("carlos");
-	unset_env("testttt");
-	display_env();
-
-	clean_env();
+		g_head = push_front(g_head, new_elem(environ[--i]));
 }
---
+++ srcs/env_controller_ex.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env_controller_ex.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bchaleil <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/05/02 11:22:19 by bchaleil          #+#    #+#             */
+/*   Updated: 2016/05/02 14:03:22 by bchaleil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "42sh.h"
+
+t_env	*g_head;
+
+void	clean_env(void)
+{
+	t_env	*tmp;
+
+	while (g_head)
+	{
+		free(g_head->key);
+		free(g_head->value);
+		tmp = g_head->next;
+		free(g_head);
+		g_head = tmp;
+	}
+}
+
+void	display_env(void)
+{
+	t_env	*cpy;
+
+	cpy = g_head;
+	while (cpy)
+	{
+		ft_putstr(cpy->key);
+		ft_putchar('=');
+		ft_putendl(cpy->value);
+		cpy = cpy->next;
+	}
+}
+
+void	set_env(char *key, char *value, int overwrite)
+{
+	t_env	*cpy;
+
+	cpy = g_head;
+	while (cpy)
+	{
+		if (overwrite && ft_strcmp(cpy->key, key) == 0)
+		{
+			free(cpy->value);
+			cpy->value = ft_strdup(value);
+			return ;
+		}
+		cpy = cpy->next;
+	}
+	if (!(cpy = ft_memalloc(sizeof(*cpy))))
+		return ;
+	cpy->key = ft_strdup(key);
+	cpy->value = ft_strdup(value);
+	cpy->next = g_head;
+	g_head = cpy;
+}
+
+int		get_env(char *key)
+{
+	t_env	*cpy;
+
+	cpy = g_head;
+	while (cpy)
+	{
+		if (ft_strcmp(cpy->key, key) == 0)
+		{
+			ft_putendl(cpy->value);
+			return (1);
+		}
+		cpy = cpy->next;
+	}
+	return (0);
+}
+
+int		unset_env(char *key)
+{
+	t_env	*cpy;
+	t_env	*prev;
+
+	cpy = g_head;
+	prev = NULL;
+	while (cpy)
+	{
+		if (ft_strcmp(cpy->key, key) == 0)
+		{
+			if (prev == NULL)
+				g_head = g_head->next;
+			else
+				prev->next = cpy->next;
+			free(cpy->key);
+			free(cpy->value);
+			free(cpy);
+			return (1);
+		}
+		prev = cpy;
+		cpy = cpy->next;
+	}
+	return (0);
+}
