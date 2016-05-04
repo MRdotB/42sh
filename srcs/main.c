e6857ddf7503f27294e391f74549be1171337a64
Baptiste CHALEIL <bchaleil@e3r4p3.42.fr>
2016-05-04 13:25:58 +0200

    remove old main

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,11 @@
 /*   By: bchaleil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/29 12:42:18 by bchaleil          #+#    #+#             */
-/*   Updated: 2016/04/29 19:01:53 by bchaleil         ###   ########.fr       */
+/*   Updated: 2016/05/04 12:59:03 by bchaleil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "42sh.h"
+#include "bcsh.h"
 
 int		main(int ac, char **av, char **env)
 {
@@ -25,43 +25,3 @@ int		main(int ac, char **av, char **env)
 		router(line);
 	return (0);
 }
-
-/*
-int		main(int ac, char **av)
-{
-	int		fd;
-	char	*line;
-	char	**args;
-	int		ret;
-	pid_t	fat;
-
-	fd = 0;
-	ret = 1;
-	if (ac != 1 && av[0])
-		return (0);
-	while (write(1, "$> ", 3) && (ret = ft_gnl(0, &line)) != 0)
-	{
-		if (!ft_strcmp("exit", line))
-			exit (-1);
-		fat = fork();
-		if (fat)
-			wait(&fat);
-		else
-		{
-			if (line[0] == 'l' && line[1] == 's')
-			{
-				args = ft_strsplit(line, ' ');
-				execve("/bin/ls", args, NULL);
-			}
-			else if (line[0] == 'c' && line[1] == 'd')
-			{
-				args = ft_strsplit(line, ' ');
-				chdir(args[1]);
-				printf("%s\n", args[1]);
-			}
-		}
-	}
-	close(fd);
-	return (0);
-}
-*/
