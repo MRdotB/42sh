477eb816414d4a4b6175afbffe64bfbba3738a38
Baptiste CHALEIL <bchaleil@e3r4p3.42.fr>
2016-05-04 18:35:14 +0200

    shellception

--- srcs/main.c
+++ srcs/main.c
@@ -6,13 +6,29 @@
 /*   By: bchaleil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/29 12:42:18 by bchaleil          #+#    #+#             */
-/*   Updated: 2016/05/04 12:59:03 by bchaleil         ###   ########.fr       */
+/*   Updated: 2016/05/04 18:34:57 by bchaleil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bcsh.h"
 
-int		main(int ac, char **av, char **env)
+static void	shellception(void)
+{
+	char	*lvl;
+	char	*itoa;
+
+	lvl = get_env("SHLVL");
+	if (lvl)
+	{
+		itoa = ft_itoa(ft_atoi(lvl) + 1);
+		set_env("SHLVL", itoa);
+		free(itoa);
+	}
+	else
+		set_env("SHLVL", "0");
+}
+
+int			main(int ac, char **av, char **env)
 {
 	char	*line;
 	int		prompt_size;
@@ -20,6 +36,7 @@ int		main(int ac, char **av, char **env)
 	if (ac && *av)
 		env_init(env);
 	hash_binaries();
+	shellception();
 	prompt_size = ft_strlen(PROMPT);
 	while (write(1, PROMPT, prompt_size) && (ft_gnl(0, &line) != 0))
 		router(line);
